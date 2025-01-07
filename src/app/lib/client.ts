d9661c7cc2a1a490e2945f26b50ea91f265af271
giahuyday <huyhoangnguyen301202@gmail.com>
2025-01-08 04:58:00 +0700

    feat: Add authorization for graphql

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,10 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const createApolloClient = () => {
-    return new ApolloClient({
+function createApolloClient() {
+    const httpLink = createHttpLink({
         uri: process.env.NEXT_PUBLIC_API_URL,
+    });
+
+    const authLink = setContext((_, { headers }) => {
+        return {
+            headers: {
+                ...headers,
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_PERM}`,
+            },
+        };
+    });
+
+    const client = new ApolloClient({
+        link: authLink.concat(httpLink),
         cache: new InMemoryCache(),
     });
-};
+
+    return client;
+}
 
 export default createApolloClient;
