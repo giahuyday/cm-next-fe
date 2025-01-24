195cea86a51cf2fe2751039fa78d2737b6516c1b
giahuyday <huyhoangnguyen301202@gmail.com>
2025-01-24 12:45:55 +0700

    feat(login): Complete login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,37 @@
 export default function Login() {
     return (
         <>
-            <div className="w-full h-[100vh]">
-                <form action="" id="login-form" className="h-full w-full flex justify-center items-center">
-                    <div className="block">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="" id="username" placeholder="Username...." />
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="" id="password" placeholder="Password...." />
-                    </div>
-                </form>
-                <h1>Login</h1>
+            <div className="w-full h-[100vh] flex justify-center items-center">
+                <div className="justify-center">
+                    <form
+                        action=""
+                        id="login-form"
+                        className="grid grid-rows-1 gap-4 backdrop:blur-xl p-20 rounded border xs:border-0 sm:border"
+                    >
+                        <h1 className="text-xl bold">Login</h1>
+                        <div className="grid">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="text"
+                                name=""
+                                className="p-2 rounded"
+                                id="username"
+                                placeholder="Username...."
+                            />
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                name=""
+                                className="p-2 rounded"
+                                id="password"
+                                placeholder="Password...."
+                            />
+                        </div>
+                        <button type="submit" className="btn-primary p-2">
+                            Login
+                        </button>
+                    </form>
+                </div>
             </div>
         </>
     );
